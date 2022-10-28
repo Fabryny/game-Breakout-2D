d38b5fc89ad41cf5c2f70d9d4d9750de69bf5ba6
Matheus <mfabryny@minsait.com>
2022-10-27 23:07:45 -0300

    Level update

--- src/LevelMaker.lua
+++ src/LevelMaker.lua
@@ -74,7 +74,6 @@ function LevelMaker.createMap(level)
                 * 32                    -- multiply by 32, the brick width
                 + 8                     -- the screen should have 8 pixels of padding; we can fit 13 cols + 16 pixels total
                 + (13 - numCols) * 16,  -- left-side padding for when there are fewer than 13 columns
-               
                 -- y-coordinate
                 y * 16                  -- just use y * 16, since we need top padding anyway
             ) 
